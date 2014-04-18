c171266148663c1069af8be329034e3263a511a5
martinnisser <mnisser@live.se>
2014-04-18 19:00:23 +0200

    Excercise 3.2
    First Matlab Commit from windows

---
+++ Control Systems 2/Ex3_2.m
@@ -0,0 +1,23 @@
+% Control 2 ; Exercise 3
+
+
+
+R = 1;
+L = 0.03;
+Ka = 0.1;
+Km = 0.1;
+J = 0.0005;
+B = 0.0002;
+
+s = tf('s');
+
+
+TF = Km / ( s * (J*s + B) * (L*s + R + Ka*Km) );
+
+sisotool(TF)
+
+margin(TF)
+
+figure
+
+margin(TF*0.02)
